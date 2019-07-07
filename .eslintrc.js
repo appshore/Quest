643802b7c1c289c72f2824d12d8b234370050e02
AppShore <git@appshore.com>
2019-07-07 20:13:07 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "arrow-parens": [2, 'as-needed'],
+        "curly": [2, 'all'],
+        'brace-style': [2, '1tbs', { allowSingleLine: false }],
+        'camelcase': [2, { ignoreDestructuring: true }],
+        'comma-dangle': [2, 'never'],
+        'comma-spacing': 1,
+        'no-confusing-arrow': 0,
+    }
+};
